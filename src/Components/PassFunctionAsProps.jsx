4159f376ffbf25003d679b6b3d0185e02127f85b
bilal <bilal@BILALJAVED>
2024-03-26 22:13:52 +0500

    change javascript extensions

--- src/Components/PassFunctionAsProps.jsx
+++ src/Components/PassFunctionAsProps.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 export default function PassFunctionAsProps() {
+  function GetData() {
+    alert("hello bilal");
+  }
+
   return (
-    <div className='App'>PassFunctionAsProps</div>
-  )
+    <div className="App">
+      Pass Function As Props
+      <Child data={GetData} />
+    </div>
+  );
 }
 
+function Child(props) {
+  return (
+    <div>
+      <h1>Child component</h1>
+      <button onClick={() => props.data()}>click</button>
+    </div>
+  );
+}
+
+/**
+ * we can pass function as props to child component
+ * if we have multiple child components in that way we can pass from the parant component to other components
+ */
